40de4c17077236245851ada6cf9096ee6e411ff6
Christian <hhvida@gmail.com>
2025-10-23 12:25:24 -0600

    Remove OptometriaLaboratorio model definition

--- models/laboratorio.py
+++ models/laboratorio.py
@@ -1,15 +1,1 @@
-# -*- coding: utf-8 -*-
 
-from odoo import models, fields
-
-class OptometriaLaboratorio(models.Model):
-    _name = 'optometria.laboratorio'
-    _description = 'Laboratorios Proveedores de Lentes'
-    _order = 'name'
-    
-    name = fields.Char(string='Nombre del Laboratorio', required=True)
-    telefono = fields.Char(string='Teléfono')
-    email = fields.Char(string='Email')
-    dias_entrega = fields.Integer(string='Días de Entrega', default=5)
-    activo = fields.Boolean(string='Activo', default=True)
-    notas = fields.Text(string='Notas')
